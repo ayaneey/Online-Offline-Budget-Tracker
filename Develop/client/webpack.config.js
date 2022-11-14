7da67ceb27be3e10171e5aabfe1e66c9aa06ab6a
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-14 02:00:47 +0000

    Creating a manifest.json file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -28,6 +28,24 @@ module.exports = () => {
 				swSrc: "./src-sw.js",
 				swDest: "src-sw.js",
 			}),
+			// Creates a manifest.json file
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: "Just Another Text Editor",
+				short_name: "JATE",
+				description: "Just Another Text Editor",
+				background_color: "#225ca3",
+				theme_color: "#225ca3",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+					},
+				],
+			}),
 		],
 
 		module: {
