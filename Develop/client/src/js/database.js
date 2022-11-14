e2f2a625f466ffa7ca16b23b82741ec6a34ebfa7
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-14 01:32:18 +0000

    set up database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,6 +14,7 @@ const initdb = async () =>
 	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// Exporting a function to POST to the db
 export const putDb = async (content) => {
 	console.log("Submit to database");
 	const jateDB = await openDB("jate", 1); // establishes a connection to the db and which version we want to use
@@ -25,11 +26,18 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
+// Exporting a function to GET to the db
 export const getDb = async () => {
 	console.log("GET from the database");
-};
+	const jateDB = await openDB("jate", 1); // again, creating a new connection to db & establishing what version to use
+	const tx = jateDB.transaction("jate", "readonly");
+	const store = tx.objectStore("jate");
+	const request = store.getAll(); // Using .getAll() method to retrieve data in the db
 
-// Form a connection to the database as well as desired version to use.
-const jateDB = await openDB("jate", 1);
+	// GET confirmation of the request
+	const result = await request;
+	console.log("result.value", result);
+	return results;
+};
 
 initdb();
