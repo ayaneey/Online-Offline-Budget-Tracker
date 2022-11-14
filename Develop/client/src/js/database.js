5592cf7b0ce7b33c47352c574f9f283f32ee852c
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-14 01:12:03 +0000

    refactored some code

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,12 +14,22 @@ const initdb = async () =>
 	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
-console.log("Submit to database");
-const jateDB = await openDB("jate", 1);
-const tx = jateDB.transaction("");
+export const putDb = async (content) => {
+	console.log("Submit to database");
+	const jateDB = await openDB("jate", 1); // establishes a connection to the db and which version we want to use
+	const tx = jateDB.transaction("jate", "rewrite");
+	const store = tx.objectStore("jate"); // selects the intended object store
+	const request = store.add({ id: 1, value: content });
+	const result = await request;
+	console.log("🚀 - data is saved to the database", result); // confirmation of the request!
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+	console.log("GET from the database");
+};
+
+// Form a connection to the database as well as desired version to use.
+const jateDB = await openDB("jate", 1);
 
 initdb();
